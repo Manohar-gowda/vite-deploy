8ed4022536ce4bdc5b1575421377c1e36e33b042
Manohar <gowdamanohar45@gmail.com>
2024-11-23 00:43:00 +0530

    Made Changes to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import React from "react";
-import { Route, Routes } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import HomePage from "./components/HomePage";
 
 function App() {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/");
+  }, []);
+
   return (
     <div>
       <Routes>
