5a6a8b2c63a1eb7a4b31f076377a96df78cbc07f
Manohar <gowdamanohar45@gmail.com>
2024-11-23 14:41:24 +0530

    update NavBar.jsx

--- README.md
+++ README.md
@@ -7,5 +7,13 @@ Currently, two official plugins are available:
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 
-
+-To run
+npm run dev
 npx json-server --watch db.json --port 3001
+
+To git changes
+git init
+git add .
+git commit -m "Display what changes"
+git push origin main
+npm run build
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import img from '../images/logovrv-.png'
+import img from '../images/WARRIOR.png'
 
 
 const NavBar = () => {
@@ -13,13 +13,13 @@ const NavBar = () => {
     <img
       src={img}
       alt="Icon"
-      className="h-[5%] w-[5%] cursor-pointer hover:scale-110 transition-transform duration-300"
+      className="h-[35%] w-[35%] cursor-pointer hover:scale-110 transition-transform duration-300"
       onClick={() => navigate("/")}
     />
   </div>
 
   {/* Title Section */}
-  <h1 className="text-2xl font-bold text-white absolute left-1/2 transform -translate-x-1/2">
+  <h1 className="text-3xl font-bold text-white absolute left-1/2 transform -translate-x-1/2">
     Admin Panel
   </h1>
 
